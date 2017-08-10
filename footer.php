7026302b7e04bdc8d692b9542ab3026426077309
jackhhh <mj@etengpc.com>
2017-08-11 00:59:48 +0800

    first commit

---
+++ footer.php
@@ -0,0 +1,53 @@
+<?php if ( 'on' == et_get_option( 'divi_back_to_top', 'false' ) ) : ?>
+
+	<span class="et_pb_scroll_top et-pb-icon"></span>
+
+<?php endif;
+
+if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
+
+			<footer id="main-footer">
+				<?php get_sidebar( 'footer' ); ?>
+
+
+		<?php
+			if ( has_nav_menu( 'footer-menu' ) ) : ?>
+
+				<div id="et-footer-nav">
+					<div class="container">
+						<?php
+							wp_nav_menu( array(
+								'theme_location' => 'footer-menu',
+								'depth'          => '1',
+								'menu_class'     => 'bottom-nav',
+								'container'      => '',
+								'fallback_cb'    => '',
+							) );
+						?>
+					</div>
+				</div> <!-- #et-footer-nav -->
+
+			<?php endif; ?>
+
+				<div id="footer-bottom">
+					<div class="container clearfix">
+				<?php
+					if ( false !== et_get_option( 'show_footer_social_icons', true ) ) {
+						get_template_part( 'includes/social_icons', 'footer' );
+					}
+				?>
+
+						<p id="footer-info">© 1905-<?php echo date("Y");?> <a href="https://www.lovefz.net">湖南师大附中·校友回访团</a> 版权所有. | <a href="http://www.jackh.cn" target="_blank">Jack.H</a> & P.God & <a href="https://ohhere.xyz" target="_blank">Graphite</a> Technical Support.</p>
+<p id="icp-info"><a href="http://www.miitbeian.gov.cn/" rel="external nofollow" target="_blank"><?php echo get_option( 'zh_cn_l10n_icp_num' );?></a></p>
+					</div>	<!-- .container -->
+				</div>
+			</footer> <!-- #main-footer -->
+		</div> <!-- #et-main-area -->
+
+<?php endif; // ! is_page_template( 'page-template-blank.php' ) ?>
+
+	</div> <!-- #page-container -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
